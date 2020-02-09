1d6a8275b763adc728c11f427f1a980b896bd5e7
caiquegl <arq.caique@hotmail.com>
2020-02-09 17:05:59 -0300

    hora adicionada no display, adicionando click

---
+++ scripts/controler/calcControler.js
@@ -0,0 +1,73 @@
+class CalcControler {
+
+    constructor() {
+
+        this._locale = "pt-BR"
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize(){
+
+        this.setDisplayTime();
+
+        setInterval(()=>{
+           
+            this.setDisplayTime();
+
+        }, 1000);
+
+
+ 
+    }
+
+    setDisplayTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: "2-digit",
+            month: "2-digit",
+            year: "2-digit"
+        })
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+    }
+
+}
